78381044a27ce456d8b61b11cb0c174982a0a72f
marcebdev <20135668+marcebdev@users.noreply.github.com>
2020-03-11 11:12:36 -0700

    isolated PageNav component and generalized modding.svelte

---
+++ src/components/PageNav.svelte
@@ -0,0 +1,95 @@
+<script>
+  // Imports from packages
+  import { getContext } from 'svelte'
+  import Icon from "fa-svelte";
+  import {
+    faCaretDown,
+    faAngleLeft,
+    faAngleRight
+  } from "@fortawesome/free-solid-svg-icons";
+  import classNames from "classnames";
+
+  // Import Functions
+  import { capitalize } from "../util/misc.js";
+
+  // Helper functions
+  // Bound Helper Functions
+  function nextStep() {
+    // Moves to next Step
+    if (!isEnd) currentStep.update(n => n + 1);
+  }
+
+  function prevStep() {
+    // Moves to previous Step
+    if (!isBeginning) currentStep.update(n => n - 1);
+  }
+
+  // Context
+  const { getCurrentStep } = getContext('modSteps');
+  let currentStep = getCurrentStep();
+
+  // States
+  $: isBeginning = $currentStep <= 0;
+  $: isEnd = $currentStep >= pageList.length - 1;
+
+  // Props
+  export let pageList;
+</script>
+
+<style lang="sass">
+  #page-nav
+    margin-top: 4.5rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+
+  // Workround for browsers that don't have the gap property
+  // TODO: make this a rollback only if browser doesn't suppoer gap
+  #page-nav > * + *
+    margin-left: 1.5rem;
+
+  .hide-button
+    visibility: hidden;
+
+  .select-wrapper
+    display: flex;
+    align-items: center;
+    position: relative;
+
+  .select-fix
+    padding-left: 5px;
+    padding-right: 35px;
+
+  //FIXME: cleaner way to do this? put icon in div/span?
+  div :global(.select-icon)
+    position: absolute;
+    right: 8px;
+</style>
+
+<div id="page-nav">
+  <button
+    class={classNames('clean-button item-block button-block-icon', {
+      'hide-button': isBeginning
+    })}
+    on:click|preventDefault={prevStep}>
+    <Icon class="fix-icon" icon={faAngleLeft} />
+  </button>
+
+  <div class="select-wrapper select">
+    <select class="clean-select item-block select-fix" bind:value={currentStep}>
+      {#each pageList as page, i}
+        <!-- FIXME: svg arrow isn't part of clickable element-->
+        <option value={i}>{i + 1 + ' - ' + capitalize(page.name)}</option>
+      {/each}
+    </select>
+    <Icon class="select-icon" icon={faCaretDown} />
+  </div>
+
+  <button
+    class={classNames('clean-button item-block button-block-icon', {
+      'hide-button': isEnd
+    })}
+    on:click|preventDefault={nextStep}>
+    <Icon class="fix-icon" icon={faAngleRight} />
+  </button>
+</div>
--- src/routes/modding.svelte
+++ src/routes/modding.svelte
@@ -1,38 +1,24 @@
 <script>
   // Imports from packages
-  import Icon from "fa-svelte";
-  import {
-    faCaretDown,
-    faAngleLeft,
-    faAngleRight
-  } from "@fortawesome/free-solid-svg-icons";
-  import classNames from "classnames";
+  import { setContext, onMount } from 'svelte'
+  import { writable } from 'svelte/store'
 
   // Import Components
   import Mod from "../components/Mod.svelte";
-
-  // Import Functions
-  import { capitalize } from "../util/misc.js";
+  import PageNav from "../components/PageNav.svelte";
 
   // Import Data
   import { modInstructions } from "../../content/mod_config.js";
   import { modList } from "../../content/mod_data.js";
 
+  let pageList = modList.map((modType, index) => ({
+    name: modType.name,
+    link: `step-${index + 1}`
+  }));
+
   // States
-  let currentStep = 0;
-  $: isBeginning = currentStep <= 0;
-  $: isEnd = currentStep >= modList.length - 1;
-
-  // Bound Helper Functions
-  function nextStep() {
-    // Moves to next Step
-    if (!isEnd) currentStep++;
-  }
-
-  function prevStep() {
-    // Moves to previous Step
-    if (!isBeginning) currentStep--;
-  }
+  const currentStep = writable(0);
+  setContext('modSteps', { getCurrentStep: () => currentStep });
 </script>
 
 <style lang="sass">
@@ -40,33 +26,7 @@
     text-align: center;
     text-transform: capitalize;
 
-  #step-controls
-    margin-top: 4.5rem;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-
-  // Workround for browsers that don't have the gap property
-  // TODO: make this a rollback only if browser doesn't suppoer gap
-  #step-controls > * + *
-    margin-left: 1.5rem;
-
-  .hide-button
-    visibility: hidden;
-
-  .select-wrapper
-    display: flex;
-    align-items: center;
-    position: relative;
-
-  .select-fix
-    padding-left: 5px;
-    padding-right: 35px;
-
-  //FIXME: cleaner way to do this? put icon in div/span?
-  div :global(.select-icon)
-    position: absolute;
-    right: 8px;
+  p
 </style>
 
 <svelte:head>
@@ -75,12 +35,12 @@
 
 <section id="mods">
   <!-- This is the title for the curren section's step -->
-  <h1 id="step-title">Step {currentStep + 1} - {modList[currentStep].name}</h1>
+  <h1 id="step-title">Step {$currentStep + 1} - {modList[$currentStep].name}</h1>
 
   <!-- This handles displaying all current mods in step or an exception otherwise -->
-  {#if modList[currentStep].hasOwnProperty('mods') && modList[currentStep].mods.length > 0}
-    {#each modList[currentStep].mods as mod, i}
-      <Mod modIndex={`${currentStep + 1}.${i + 1}`} {mod} {modInstructions} />
+  {#if modList[$currentStep].hasOwnProperty('mods') && modList[$currentStep].mods.length > 0}
+    {#each modList[$currentStep].mods as mod, i}
+      <Mod modIndex={`${$currentStep + 1}.${i + 1}`} {mod} {modInstructions} />
     {/each}
   {:else}
     <p>
@@ -88,35 +48,6 @@
       error, please inform a team member!
     </p>
   {/if}
-
-  <!-- This is the selection menu for navigating the steps -->
-  <div id="step-controls">
-    <button
-      class={classNames('clean-button item-block button-block-icon', {
-        'hide-button': isBeginning
-      })}
-      on:click|preventDefault={prevStep}>
-      <Icon class="fix-icon" icon={faAngleLeft} />
-    </button>
-
-    <div class="select-wrapper select">
-      <select
-        class="clean-select item-block select-fix"
-        bind:value={currentStep}>
-        {#each modList as step, i}
-          <!-- TODO: svg arrow isn't part of clickable element -->
-          <option value={i}>{i + 1 + ' - ' + capitalize(step.name)}</option>
-        {/each}
-      </select>
-      <Icon class="select-icon" icon={faCaretDown} />
-    </div>
-
-    <button
-      class={classNames('clean-button item-block button-block-icon', {
-        'hide-button': isEnd
-      })}
-      on:click|preventDefault={nextStep}>
-      <Icon class="fix-icon" icon={faAngleRight} />
-    </button>
-  </div>
 </section>
+
+<PageNav {pageList} />
